bc6b40440dcd7ab8074f199614503f21df29a042
Yaaqoub <semlali.yaaqoub@gmail.com>
2020-11-15 20:58:11 +0100

    initial setup of swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
+
+  const options = new DocumentBuilder()
+    .setTitle('Users Management API')
+    .setDescription('All users CRUD operations!')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/help', app, document);
+
   await app.listen(configService.get('port'));
 }
 
